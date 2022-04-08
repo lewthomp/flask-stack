5a01c2fa7926e3d7bf8c9fce66cf680ad373c8e2
rvdemonk <rvdemonk@gmail.com>
2022-04-08 21:16:14 +1000

    up to views

--- app/__init__.py
+++ app/__init__.py
@@ -1,44 +1,38 @@
 import os
-from flask import Flask
+from flask import Flask, g
+from flask_pymongo import PyMongo
+from . import auth
 
 
 def create_app(test_config=None):
-    """
-    Application factory function.
-    Creates Flask instance, sets default configuration, leaving room for 
-    overriding.
-    Ensures instance path exists to contain SQLite database.
-    Creates simple /hello route to test application.
-
-    Bash:
-    $ export FLASK_APP=flaskr
-    $ export FLASK_ENV=development
-    $ flask run
-    """
-    # create and configure app
-    print("Name: ", __name__)
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY = 'dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.')
+        SECRET_KEY="dev",
     )
-
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/demo"
+    mongo = PyMongo(app)
+    # set flask database
+    g.db = mongo.db
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    #ensure the instance folder exists
+    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    #a simple page that says hello
-    @app.route('/hello')
+    app.register_blueprint(auth.bp)
+
+    # a simple page that says hello
+    @app.route("/hello")
     def hello():
-        return 'Hello cunts'
-    
+        post = mongo.db.posts.find_one()
+        print(post)
+        return f"{post}"
+
     return app
