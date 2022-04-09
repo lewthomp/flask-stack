0dd21af6fa377b90d219fafc2b88614d51b92935
rvdemonk <rvdemonk@gmail.com>
2022-04-09 15:41:10 +1000

    smol update

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, g, current_app
 from flask_pymongo import PyMongo
+from . import db
 
 
 def create_app(test_config=None):
@@ -10,21 +11,16 @@ def create_app(test_config=None):
     overriding.
     Ensures instance path exists to contain SQLite database.
     Creates simple /hello route to test application.
-
     Bash:
-    $ export FLASK_APP=flaskr
+    $ export FLASK_APP=app
     $ export FLASK_ENV=development
     $ flask run
     """
     print("\n** Initialising app\n")
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY="dev",
-    )
-    app.config["MONGO_URI"] = "mongodb://localhost:27017/"
-    
-    # databse stuff
-    mongo = PyMongo(app)
+    app.config.from_mapping(SECRET_KEY="dev")
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/demo"
+    db.init_app(app)
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
@@ -32,18 +28,16 @@ def create_app(test_config=None):
         app.config.from_mapping(test_config)
 
     print("* Flask app object:", type(app), app)
-    # print("* Flask current_app object:", type(current_app), current_app)
-    # print("* Are these equivalent?", (app == current_app))
-
-    #ensure the instance folder exists
+    
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    #a simple page that says hello
     @app.route('/test')
     def hello():
+        print(g.db)
         return "Hello cunts!"
     
+
     return app
--- app/db.py
+++ app/db.py
@@ -1,7 +1,30 @@
-<<<<<<< HEAD
-=======
-from flask import current_app, g
+import click
 from flask_pymongo import PyMongo
+from flask import current_app, g
+from flask.cli import with_appcontext
 
 
->>>>>>> 5a01c2fa7926e3d7bf8c9fce66cf680ad373c8e2
+def get_db():
+    if 'db' not in g:
+        g.db = PyMongo(current_app)
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        # dont think this works with mongo
+        db.close()
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables"""
+    db = get_db()
+    click.echo('Initialized the database')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
